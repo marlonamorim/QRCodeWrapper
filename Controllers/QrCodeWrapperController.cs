960e03e35ea78176bcaf7ac9c3face5b7419b00e
Marlon Amorim <marlon.amorim@sitemercado.com.br>
2021-02-15 20:07:37 -0300

    Add project files.

---
+++ Controllers/QrCodeWrapperController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QrCodeWrapper.Services;
+
+namespace QrCodeWrapper.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QrCodeWrapperController : ControllerBase
+    {
+        private readonly ILogger<QrCodeWrapperController> _logger;
+
+        public QrCodeWrapperController(ILogger<QrCodeWrapperController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get(string url)
+        {
+            var image = QrCodeGeneratorService.GenerateByteArray(url);
+            return File(image, "image/jpeg");
+        }
+    }
+}
